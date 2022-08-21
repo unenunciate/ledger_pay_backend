2eb289e20ffafae31b0d12abb20c87141a059045
unenunciate <pmkelley@protonmail.com>
2022-08-21 18:56:24 -0400

    stytch...

--- src/api/stytch/controllers/stytch.js
+++ src/api/stytch/controllers/stytch.js
@@ -10,16 +10,16 @@ module.exports = createCoreController('api::stytch.stytch', ({strapi}) => ({
     findFromStytchId: async (ctx) => {
         let result = await strapi.entityService.findMany('api::stytch.stytch', {
             feilds: ['id', 'stytchAuthMethod', 'authIdentifier'],
-            filters: { stytchId: ctx.request.body.stytchId },
+            filters: { stytchId: ctx.params.stytchId },
             populate: { user: true },
         });
 
-        strapi.log.info(JSON.stringify(ctx.request.body));
+        strapi.log.info(JSON.stringify(ctx.params));
 
         if(result.length < 1) {
             result = await strapi.entityService.create('api::stytch.stytch', {
-                fields: ['id', 'stytchAuthMethod', 'authIdentifier'],
-                data: { stytchId: ctx.request.body.stytchId, stytchAuthMethod: ctx.request.body.stytchAuthMethod, authIdentifier: ctx.request.body.authIdentifier },
+                fields: ['id'],
+                data: { stytchId: ctx.params.stytchId, stytchAuthMethod: ctx.params.stytchAuthMethod, authIdentifier: ctx.params.authIdentifier },
             });
         }
 
