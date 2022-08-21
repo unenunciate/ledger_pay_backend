575684a43677e1c53e6f11be18ae9e583da0b86c
unenunciate <pmkelley@protonmail.com>
2022-08-21 16:22:57 -0400

    auth

--- src/api/stytch/controllers/stytch.js
+++ src/api/stytch/controllers/stytch.js
@@ -6,7 +6,7 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::stytch.stytch', (strapi) => ({
+module.exports = createCoreController('api::stytch.stytch', ({strapi}) => ({
     findFromStytchId: async (ctx) => {
         let result = await strapi.entityService.findMany('api::stytch.stytch', {
             feilds: ['id', 'stytchAuthMethod', 'authIdentifier'],
@@ -15,14 +15,14 @@ module.exports = createCoreController('api::stytch.stytch', (strapi) => ({
         });
 
         strapi.log.info(JSON.stringify(ctx.request.body));
-
+        /** 
         if(result.length < 1) {
             result = await strapi.entityService.create('api::stytch.stytch', {
                 fields: ['id', 'stytchAuthMethod', 'authIdentifier'],
                 data: { stytchId: ctx.request.body.stytchId, stytchAuthMethod: ctx.request.body.stytchId, authIdentifier: ctx.request.body.authIdentifier },
             });
         }
-
+*/
         ctx.response.body = JSON.stringify(result);
         ctx.send();
         return { result };
