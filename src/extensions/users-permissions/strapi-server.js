7cc7563f88243906f59c77be3ebf805040a21a20
unenunciate <pmkelley@protonmail.com>
2022-08-21 11:03:31 -0400

    stytch debuging

--- src/extensions/users-permissions/strapi-server.js
+++ src/extensions/users-permissions/strapi-server.js
@@ -19,14 +19,15 @@ module.exports = (plugin) => {
 
     plugin.controllers.user.findFromStytchId = async (ctx) => {
         let result = await strapi.entityService.findMany('api::stytch.stytch', {
+            feilds: ['id', 'stytchAuthMethod', 'authIdentifier'],
             filters: { stytchId: ctx.request.body.stytchId },
             populate: { user: true },
         });
-        
+        console.log(JSON.stringify(ctx.request.body))
         if(result.length < 1) {
             result = await strapi.entityService.create('api::stytch.stytch', {
                 fields: ['id', 'stytchAuthMethod', 'authIdentifier'],
-                data: { ...ctx.request.body },
+                data: { stytchId: ctx.request.body.stytchId, stytchAuthMethod: ctx.request.body.stytchId, authIdentifier: ctx.request.body.authIdentifier },
             });
         }
 
