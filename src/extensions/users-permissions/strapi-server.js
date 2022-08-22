37d9e73f20993ad9b0a9cbaf3af7db7578d300fd
unenunciate <pmkelley@protonmail.com>
2022-08-22 04:21:33 -0400

    stytch user relation

--- src/extensions/users-permissions/strapi-server.js
+++ src/extensions/users-permissions/strapi-server.js
@@ -7,6 +7,18 @@ module.exports = (plugin) => {
                 ...ctx.request.body
             }
         });
+
+        await strapi.entityService.update('plugin::users-permissions.user', id.id, {
+            data: {
+                stytches: ctx.request.body.stytches
+            }
+        });
+
+        await strapi.entityService.update('api::stytch.stytch', ctx.request.body.stytches[0], {
+            data: {
+                user: id.id
+            }
+        });
         
         const result =  await strapi.entityService.findOne('plugin::users-permissions.user', id.id);
 
