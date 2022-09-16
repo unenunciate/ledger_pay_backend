6bd85790556d092f7e189c3e0ff02aa1af534651
alexxriv <alex.trrzrivera@gmail.com>
2022-09-15 20:22:41 -0500

    user schema fixed

--- src/index.js
+++ src/index.js
@@ -1,20 +1,8 @@
 'use strict';
 
 module.exports = {
-  /**
-   * An asynchronous register function that runs before
-   * your application is initialized.
-   *
-   * This gives you an opportunity to extend code.
-   */
-  register(/*{ strapi }*/) {},
+  register({ strapi }) {
 
-  /**
-   * An asynchronous bootstrap function that runs before
-   * your application gets started.
-   *
-   * This gives you an opportunity to set up your data model,
-   * run jobs, or perform some special logic.
-   */
-  bootstrap(/*{ strapi }*/) {},
+  },
+  bootstrap({ strapi }) { },
 };
